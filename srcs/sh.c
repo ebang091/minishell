6cc46cd2510d05aa8f49a2138e392c8ddf6e7787
youngpar <youngseo321@gmail.com>
2022-12-12 01:09:32 +0900

    [feat] Flow defining in shell

--- srcs/sh.c
+++ srcs/sh.c
@@ -1,23 +1,37 @@
 # include "../inc/minish.h"
 
+static void reset(t_stat *stat)
+{
+	stat->pipe_num = 0;
+	stat->error = -1;
+}
+
 int	minishell(t_stat *stat)
 {
 	char	*input;
 	t_lst	*parsed;
 
-	stat->pipe_num = 0;
-	stat->error = -1;
-
+	//	TODO : 시그널 세팅
 	while (1)
 	{
-		printf("%s", stat->pgname);
-		input = readline(" % ");
+		input = readline("minishell % ");
 		//if (!input)
-		//	return (
+			//	TODO : 종료
 		if (*input != '\0')
 		{
-			stat->pipe_num = 0;
-			stat->error = -1;
+			reset(stat);
+			add_history(input);
+
+			//	TODO : 파싱
+			//	TODO : 실행 or 에러 처리
+			//	TODO : 파싱 자료구조 release
+
+			/**
+			 * @brief : 최종 결과 = 실행 결과의 최종 값
+			 * @retval : 이전 결과 값 && 또는 || 현재 결과 값, CMD && CMD 등 여러 명령 처리
+			 */
+			stat->last_ret = stat->cmd_ret;
+
 			printf("[%s]\n", input);
 		}
 		free(input);
