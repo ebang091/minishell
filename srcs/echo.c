a9ee6a5389a15f029b027521d8dc95eb29f536f1
ebang091 <dskhe8@gmail.com>
2023-01-04 21:51:15 +0900

    [feat] finish echo.c

--- srcs/echo.c
+++ srcs/echo.c
@@ -4,8 +4,55 @@
 //TODO : @성공 시, 실패시 반환 값
 //궁금한 점: pipe, redirection 의 경우, stdout을 미리 변경한 후에 실행하면 되나?
 
-void echo()
-{
-   
 
+int ft_find_option(char *argv)
+{
+	int	i;
+	int cnt;
+
+	i = 1;
+	cnt = 0;
+	if(argv[0] != '-')
+		return (0);
+	while (argv[i] && argv[i] == 'n')
+	{
+			cnt++;
+			i++;
+	}
+	if(argv[i])
+		return 0;
+	return (cnt);
+}
+
+int ft_echo(int argc, char **argv)
+{
+	int	i;
+	int	flag;
+	int start;
+
+	i = 1;
+	flag = 0;
+	start = 1;
+	while(i < argc)
+	{
+		if(ft_find_option(argv[i]))
+		{
+			if(start)
+				flag = 1;					
+			else{
+				printf("%s", argv[i]);
+			}
+		}
+		else
+		{
+			printf("%s", argv[i]);
+			if (i < argc -1)
+				printf(" ");
+			start = 0;
+		}
+		i++;
+	}
+	if(!flag)
+		printf("\n");
+	return (0);
 }
