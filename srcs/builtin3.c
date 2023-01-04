8af8d901e9228767af320625f04ba40b4b043200
ebang091 <dskhe8@gmail.com>
2023-01-04 21:11:39 +0900

    [feat] create parsing error function

--- srcs/builtin3.c
+++ srcs/builtin3.c
@@ -19,3 +19,32 @@ int	ft_builtin_error4(const char *pgname, const char *cmd, const char *arg, cons
     ft_builtin_error(cmd, arg, msg);
     return (1);
 }
+
+
+int	ft_parsing_error(const char *cmd, const char *msg, const char *tok)
+{
+	ft_putstr_fd(2, cmd);
+	ft_putstr_fd(2, ": ");
+	ft_putstr_fd(2, msg);
+	ft_putstr_fd(2, " `");
+	ft_putstr_fd(2, tok);
+	ft_putstr_fd(2, "\'");
+	ft_putstr_fd(2, "\n");
+	return (PARS_ERR_RET);
+}
+
+int	ft_builtin_error_tok4(const char *pgname, const char *cmd,
+	const char *tok, const char *msg)
+{
+	ft_putstr_fd(2, pgname);
+	ft_putstr_fd(2, ": ");
+	ft_putstr_fd(2, cmd);
+	ft_putstr_fd(2, ": ");
+	ft_putstr_fd(2, "`");
+	ft_putstr_fd(2, tok);
+	ft_putstr_fd(2, "\'");
+	ft_putstr_fd(2, ": ");
+	ft_putstr_fd(2, msg);
+	ft_putstr_fd(2, "\n");
+	return (1);
+}
